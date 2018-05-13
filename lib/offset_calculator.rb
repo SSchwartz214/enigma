53ed084972297cf87620853f76fdbcc813d82afd
Seth Schwartz <sschwartz214@gmail.com>
2018-05-13 12:53:05 -0600

    moves date generation into offset calculator class and creates offset numbers

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -4,8 +4,18 @@ class OffsetCalculator
   attr_reader :date,
               :key
 
-  def initialize(date, key)
+  def initialize(date = Time.now.strftime("%d%m%y"), key)
     @date = date
     @key = key
   end
+
+  def square_and_last_four
+    date_squared = @date.to_i ** 2
+    date_string = date_squared.to_s
+    last_four = date_string[-4..-1].to_i
+    # date_squared_string =
+    # date_squared_string(-4..-1)
+    binding.pry
+  end
+
 end
--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -5,17 +5,21 @@ require './lib/date_generator'
 
 class OffsetCalulatorTest < Minitest::Test
 
-  def test_it_can_bring_in_the_generated_key_and_date
-    date_generator = DateGenerator.new
-    date = date_generator.generate_date
+  def test_it_has_a_key
+    offset_calculator = OffsetCalculator.new(130518, [9, 3, 0, 5, 6])
 
-    key_generator = KeyGenerator.new
-    key = key_generator.generate_key
-
-    offset_calculator = OffsetCalculator.new(date, key)
-
-    assert Integer, offset_calculator.date
     assert_equal 5, offset_calculator.key.length
   end
 
+  def test_it_can_generate_current_date
+    offset_calculator = OffsetCalculator.new(130518, [9, 3, 0, 5, 6])
+
+    assert Integer, offset_calculator.date
+  end
+
+  def test_it_can_square_and_take_last_4_digits_of_date
+    offset_calculator = OffsetCalculator.new(130518, [9, 3, 0, 5, 6])
+
+    assert_equal 412699225, offset_calculator.square_and_last_four
+  end
 end
