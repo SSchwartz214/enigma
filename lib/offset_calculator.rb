2def229f30957152f6ce62deb571bfaaca4c4b31
Seth Schwartz <sschwartz214@gmail.com>
2018-05-15 17:02:11 -0600

    changes output to array

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -1,17 +1,18 @@
 require 'date'
 
 class OffsetCalculator
-  attr_reader :date
+  attr_reader :offset_values
 
-  def initialize(date = Time.now.strftime("%d%m%y"))
-    @date = date.to_i
+  def initialize
+    @offset_values = generate_offset_values
   end
 
-  def offset_values
-    date_squared = @date ** 2
+  def generate_offset_values
+    date = Time.now.strftime("%d%m%y").to_i
+
+    date_squared = date ** 2
     date_string = date_squared.to_s
-    date_string[-4..-1].to_i
+    last_four = date_string[-4..-1]
+    last_four.chars.map(&:to_i)
   end
-
-
 end
