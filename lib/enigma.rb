81735489c536bdf52f91550083d3f2f037fcec4d
Seth Schwartz <sschwartz214@gmail.com>
2018-05-15 17:05:28 -0600

    deletes old code

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -22,27 +22,11 @@ class Enigma
 
   def shift(index)
     @key[index] + @offset_values[index]
-    # binding.pry
   end
 
-
-  # def shifted_character_map_hash(shift)
-  #   shifted_character_map(shift)
-  # end
-
-    def message_encryptor(char, index)
-      shifted_character_map(index % 4)[char]
-      # if index % 4 == 0
-      #   char = shifted_character_map_hash[char]
-      # elsif index % 4 == 1
-      #   char = shifted_character_map_hash[char]
-      # elsif index % 4 == 2
-      #   char = shifted_character_map_hash[char]
-      # elsif index % 4 == 3
-      #   char = shifted_character_map_hash[char]
-      # end
-    end
-
+  def message_encryptor(char, index)
+    shifted_character_map(index % 4)[char]
+  end
 
   def encrypt(english_text)
     message = english_text.split("").map.with_index do |char, index|
@@ -50,59 +34,4 @@ class Enigma
     end
     message.join
   end
-
-  # def a_rotation
-  #   a_rotation = []
-  #   a_rotation << @key[0] << @key[1]
-  #   a_rotation.join.to_i
-  # end
-  #
-  # def b_rotation
-  #   b_rotation = []
-  #   b_rotation << @key[1] << @key[2]
-  #   b_rotation.join.to_i
-  # end
-  #
-  # def c_rotation
-  #   c_rotation = []
-  #   c_rotation << @key[2] << @key[3]
-  #   c_rotation.join.to_i
-  # end
-  #
-  # def d_rotation
-  #   d_rotation = []
-  #   d_rotation << @key[3] << @key[4]
-  #   d_rotation.join.to_i
-  # end
-
-
-  # def b_shift
-  #   @offset_values[1] + b_rotation
-  # end
-  #
-  # def c_shift
-  #   @offset_values[2] + c_rotation
-  # end
-  #
-  # def d_shift
-  #   @offset_values[3] + d_rotation
-  # end
-
-  # def shifted_character_map_hash_b
-  #   shifted_character_map(b_shift)
-  # end
-  #
-  # def shifted_character_map_hash_c
-  #   shifted_character_map(c_shift)
-  # end
-  #
-  # def shifted_character_map_hash_d
-  #   shifted_character_map(d_shift)
-  # end
 end
-
-
-
-# enigma = Enigma.new
-# english_text = "turing is awesome."
-# p enigma.encrypt(english_text)
