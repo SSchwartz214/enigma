d00d75282845ac1b6b936c3501ec432ef3e2750a
Seth Schwartz <sschwartz214@gmail.com>
2018-05-15 17:01:12 -0600

    changes ouput to array

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -2,6 +2,16 @@ class KeyGenerator
   attr_reader :key
 
   def initialize
-    @key = rand(00000..99999).to_s.rjust(5, '0')
+    @key = generate_key
+  end
+
+  def generate_key
+    random_number = rand(00000..99999).to_s.rjust(5, '0').chars
+
+    key = []
+    4.times do |index|
+      key << (random_number[index] + random_number[index + 1]).to_i
+    end
+    key
   end
 end
