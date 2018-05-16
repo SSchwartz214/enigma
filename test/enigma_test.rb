3ddc20b94f86a85dfb7a9932b97e090cb318d188
andrewetobin <andrewetobin@gmail.com>
2018-05-15 19:03:16 -0600

    adds updates to enigma test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,7 +6,7 @@ require './lib/offset_calculator'
 class EnigmaTest < Minitest::Test
 
   def test_it_exists
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
     assert_instance_of Enigma, enigma
   end
@@ -19,67 +19,55 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_has_character_map
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
     assert_equal (("a".."z").to_a + ("0".."9").to_a + [" ", ".", ","]), enigma.character_map
   end
 
-  def test_it_can_generate_rotation_keys
-    skip
-    key_generator = KeyGenerator.new
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+  def test_it_can_convert_character_map_to_a_hash
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
-    assert_equal 41, enigma.a_rotation
-    assert_equal 15, enigma.b_rotation
-    assert_equal 52, enigma.c_rotation
-    assert_equal 21, enigma.d_rotation
+    assert_equal 39, enigma.shifted_character_map(0).count
+    assert_instance_of Hash, enigma.shifted_character_map(0)
+  end
+
+  def test_it_can_create_a_shift_number
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
+
+    assert_instance_of Integer, enigma.shift(3)
   end
 
   def test_it_can_generate_shifts
-    skip
     key_generator = KeyGenerator.new
-    offset_calculator = OffsetCalculator.new('0%o' % 020315)
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+    # offset_calculator = OffsetCalculator.new.offset_values
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
-    assert_equal 50, enigma.a_shift
-    assert_equal 17, enigma.b_shift
-    assert_equal 54, enigma.c_shift
-    assert_equal 26, enigma.d_shift
-  end
-
-  def test_it_can_convert_character_map_to_a_hash
-skip
-    key_generator = KeyGenerator.new
-    offset_calculator = OffsetCalculator.new('0%o' % 020315)
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
-
-    shift = enigma.a_shift
-    assert_equal ["s"], enigma.shifted_character_map(shift).values_at("h")
+    assert_equal 50, enigma.shift(0)
+    assert_equal 17, enigma.shift(1)
+    assert_equal 54, enigma.shift(2)
+    assert_equal 26, enigma.shift(3)
   end
 
   def test_it_can_encrypt_a_letter
-    skip
     key_generator = KeyGenerator.new
-    offset_calculator = OffsetCalculator.new('0%o' % 020315)
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+    offset_calculator = OffsetCalculator.new
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
     assert_equal "s", enigma.encrypt("h")
   end
 
   def test_it_can_encrypt_several_letters
-    skip
     key_generator = KeyGenerator.new
-    offset_calculator = OffsetCalculator.new('0%o' % 020315)
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+    offset_calculator = OffsetCalculator.new
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
     assert_equal "sv0.z", enigma.encrypt("hello")
   end
 
   def test_it_can_encrypt_two_words
-    skip
     key_generator = KeyGenerator.new
-    offset_calculator = OffsetCalculator.new('0%o' % 020315)
-    enigma = Enigma.new([4, 1, 5, 2, 1], 9225)
+    offset_calculator = OffsetCalculator.new
+    enigma = Enigma.new([41, 15, 52, 21], [9, 2, 2, 5])
 
     assert_equal "sv0.zo.b22s", enigma.encrypt("hello world")
   end
