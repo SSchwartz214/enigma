fd3f0f7516944fb375804875629b6b75bcaad67a
andrewetobin <andrewetobin@gmail.com>
2018-05-15 18:07:12 -0600

    adds updated offset calculator test

--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -4,27 +4,15 @@ require './lib/offset_calculator'
 class OffsetCalulatorTest < Minitest::Test
 
   def test_it_exists
-    offset_calculator = OffsetCalculator.new("020315")
+    offset_calculator = OffsetCalculator.new
 
     assert_instance_of OffsetCalculator, offset_calculator
   end
 
   def test_it_can_generate_current_date
-    offset_calculator = OffsetCalculator.new("020315")
-
-    assert_instance_of Integer, offset_calculator.date
-  end
-
-  def test_it_can_have_correct_current_date_by_default
     offset_calculator = OffsetCalculator.new
 
-    assert_equal Time.now.strftime("%d%m%y").to_i, offset_calculator.date
-  end
-
-  def test_it_can_square_and_take_last_4_digits_of_date
-
-    offset_calculator = OffsetCalculator.new("020315")
-
-    assert_equal 9225, offset_calculator.offset_values
+    assert_equal 4, offset_calculator.offset_values.length
+    assert_instance_of Array, offset_calculator.offset_values
   end
 end
