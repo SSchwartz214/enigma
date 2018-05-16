22d4ab6d1c49c084c4228649e812ffc59d20ba29
Seth Schwartz <sschwartz214@gmail.com>
2018-05-16 15:05:31 -0600

    adds text files and working file io

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,9 +7,11 @@ incoming_text = handle.read
 handle.close
 
 enigma = Enigma.new
+key_generator = KeyGenerator.new
+offset_calculator = OffsetCalculator.new
 
 writer = File.open(ARGV[1], "w")
-writer.write(enigma.encrypt(english_text))
+writer.write(enigma.encrypt(incoming_text))
 writer.close
 
 puts "Created 'encrypted.txt' with the key #{key_generator.key} and date #{offset_calculator.offset_values}"
--- lib/runner.rb
+++
@@ -1,9 +0,0 @@
-require './lib/key_generator'
-require './lib/date_generator'
-require './lib/offset_calculator'
-
-# key_generator = KeyGenerator.new
-# key = key_generator.generate_key
-#
-#
-# offset_calculator = OffsetCalculator.new(date) offset_calculator
